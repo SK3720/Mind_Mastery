9478cc971952bc8726c8eacbe532612affafd6a0
10111 <95386420+3xp3rtz@users.noreply.github.com>
2023-05-23 13:46:22 -0400

    added JavaDoc comments, added contributor notes

--- Mind_Mastery.java
+++ Mind_Mastery.java
@@ -1,3 +1,13 @@
+/** 
+Authors: Focus Forge - Caleb Chue and Shiv Kanade
+Teacher: V. Krasteva
+Date: May 23, 2023
+Description: The first version of Focus Forge's game, Mind Mastery. 
+             It includes a fading splashscreen, and a few buttons on
+             the main menu for user navigation. More will be implemented
+             in the next version of Mind Mastery.
+*/ 
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
@@ -8,8 +18,11 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Mind_Mastery implements ActionListener {
+    // JFrame to hold all content
     private JFrame frame;
     Drawing draw;
+    
+    // variable to hold screen size
     Dimension screenSize;
     int locx, locy;
     
@@ -42,7 +55,7 @@ public class Mind_Mastery implements ActionListener {
     // credits
     JPanel credPanel;
     
-    
+    // constructor
     public Mind_Mastery() {
         frame = new JFrame("Mind Mastery");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,14 +63,28 @@ public class Mind_Mastery implements ActionListener {
         // getting the size of the screen (source: https://stackoverflow.com/questions/8141865/java-how-to-get-the-screen-dimensions-from-a-graphics-object)
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        // loading all panels, buttons, content of the main menu
         load();
         
+        // showing the splashscreen
         splashScreen();
         
+        // displaying the main menu
         mainMenu();
     }
     
     
+    /** 
+    Private method to load content for the main menu, splashscreen,
+    level select, instruction, and credits screen, and buttons
+    for user navigation between pages
+    
+    <-------May 17------->
+      > added loading of drawing and drawing panel, main menu and associated buttons
+      > added loading of main frame, sizing, and display
+      Contributor: Caleb Chue
+    
+    */ 
     private void load() {
         // drawing 
         draw = new Drawing();
@@ -72,7 +99,10 @@ public class Mind_Mastery implements ActionListener {
         mainMenuButtons = new JPanel();
         mainMenuButtons.setLayout(new BoxLayout(mainMenuButtons, BoxLayout.Y_AXIS));
         
-        mainButtons = new JButton[] {new JButton("Begin Adventure"), new JButton("How To Play"), new JButton("Credits"), new JButton("Exit")};
+        mainButtons = new JButton[] {new JButton("Begin Adventure"), 
+                                     new JButton("How To Play"), 
+                                     new JButton("Credits"), 
+                                     new JButton("Exit")};
 
         mainMenuButtons.add(Box.createVerticalStrut(6*screenSize.height/17));
         for (int i = 0; i < mainButtons.length; i++) loadMainButton(i);
@@ -89,6 +119,18 @@ public class Mind_Mastery implements ActionListener {
         
     }
     
+    
+    /** 
+    Private method to simplify loading new buttons on the main menu
+    
+    @param index The index of the button within the array of JButtons
+    
+
+    <-------May 18------->
+      > added loading of JButtons for convenience
+      Contributor: Caleb Chue
+    
+    */ 
     private void loadMainButton(int index) {
         mainButtons[index].addActionListener(this);
         mainButtons[index].setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -101,12 +143,33 @@ public class Mind_Mastery implements ActionListener {
     }
     
     
+    /** 
+    Private method to handle the displaying of the splashscreen, including the delays for it
+
+    <-------May 16------->
+      > added displaying of splashscreen
+      > added fade out code
+      Contributor: Caleb Chue
+    
+    <-------May 18------->
+      > after integration to JPanel and individual panel, the 
+        splashscreen wouldn't fade in and would not show on screen
+      > fixed issues, fades out properly
+      Contributor: Caleb Chue
+    
+    <-------May 23------->
+      > changed background color of splashscreen
+      Contributor: Shiv Kanade
+    
+    
+    */
     private void splashScreen() {
         // splashscreen
         state = 0;
         drawPanel.setVisible(true);
         draw.setVisible(true);
         frame.setContentPane(drawPanel);
+        frame.getContentPane().setBackground(new Color(191,215,255));
         
         draw.repaint();
         sleep(1000);
@@ -115,10 +178,24 @@ public class Mind_Mastery implements ActionListener {
             sleep(5);
         }
         sleep(1000);
-        System.out.println("splash finished");
+        // System.out.println("splash finished");
     }
     
     
+    /** 
+    Private method to handle the display of the main menu
+    
+    <-------May 18------->
+      > added buttons and visibility
+      Contributor: Caleb Chue
+    
+    <-------May 19------->
+      > changed background color of main menu
+      > made main menu panel appear on top as content pane
+      Contributor: Caleb Chue
+    
+    
+    */ 
     private void mainMenu() {
         frame.setContentPane(mainMenu);
         frame.revalidate();
@@ -132,22 +209,55 @@ public class Mind_Mastery implements ActionListener {
     }
     
     
+    /** 
+    Private method to handle the display of the credits screen
+    */ 
     private void credits() {
         frame.setContentPane(credPanel);
     }
     
     
+    /** 
+    Private method to handle the display of the instructions screen
+    */ 
     private void instructions() {
         
     }
     
     
+    /** 
+    Private method to simplify delaying the program by a certain amout of time
+    
+    @param ms The number of milliseconds to delay by
+
+    <-------May 16------->
+      > added method to help sleep the program
+      Contributor: Caleb Chue
+    
+    */ 
     public void sleep(int ms) {
         try{
             Thread.sleep(ms);
         } catch(Exception e){}
     }
     
+    
+    /** 
+    Public overridden method to handle events from various sources
+    
+    @param e An action event to handle
+    
+    <-------May 16------->
+      > added exit button
+      > added redrawing on clicking on a Drawing
+      Contributor: Caleb Chue
+    
+    <-------May 17------->
+      > added functionality to other buttons from main menu, 
+        such as level select, instructions, and credits
+      Contributor: Caleb Chue
+    
+    */ 
     public void actionPerformed(ActionEvent e) {
         // main menu
         if (e.getSource() == mainButtons[0]) {
@@ -160,7 +270,6 @@ public class Mind_Mastery implements ActionListener {
             state = 4;
             mainMenuButtons.setVisible(false);
         } else if (e.getSource() == mainButtons[3]) {
-            System.out.println("please exit");
             frame.setVisible(false);
             frame.dispose();
             System.exit(0);
@@ -169,11 +278,20 @@ public class Mind_Mastery implements ActionListener {
         }
         
         
-        
         // draw.repaint();
     }
     
+    
+    /** 
+    Nested class to handle mouse events
+    */
     class ClickHandler extends MouseAdapter {
+    
+        /** 
+        Public overridden method to log the location of the last mouse click
+        
+        @param e The last mouse event
+        */
         public void mouseClicked(MouseEvent e) {
             locx = e.getX();
             locy = e.getY();
@@ -181,7 +299,32 @@ public class Mind_Mastery implements ActionListener {
         }
     }
     
+    /** 
+    Nested class to handle drawing on a canvas-like component
+    */ 
     class Drawing extends JComponent {
+    
+        /** 
+        Public overridden method to paint the component, drawing
+        different contents based on the state of the program
+        
+        @param g The graphics Object to draw on
+
+        <-------May 16------->
+          > added drawing of splashscreen, including displaying image
+          > added placeholder main menu graphic
+          Contributor: Caleb Chue
+    
+        <-------May 17------->
+          > removed placeholder graphic
+          Contributor: Caleb Chue
+    
+        <-------May 18------->
+          > added placeholder graphic for all states not handled by logic structure
+          Contributor: Caleb Chue
+        
+
+        */ 
         public void paint(Graphics g) {
             if (state == 0) { // splashscreen
                 try {
@@ -195,23 +338,21 @@ public class Mind_Mastery implements ActionListener {
                 } catch (IOException e) {
                     System.out.println("Could not draw image: " + e);
                 }
-            } else if (state == 1) { // main menu
-                g.setColor(new Color(61, 68, 128));
-                g.fillRect(0, 0, screenSize.width, screenSize.height);
-                g.setColor(new Color(255,0,0));
-                g.fillRect(100, 100, 500, 500);
-                g.fillOval(locx,locy,10,10);
-                System.out.println("Should draw main menu: " + locx + " " + locy);
             } else {
                 g.setColor(new Color(255, 0, 255));
                 g.fillRect(0, 0, screenSize.width, screenSize.height);
-                System.out.println("should draw other >:(");
+                // System.out.println("should draw other >:(");
             }
         }
     }
     
     
-    public static void main(String args[]) {
+    /** 
+    Main method
+    
+    @param args Arguments for the application
+    */ 
+    public static void main(String[] args) {
         new Mind_Mastery();
     }
 }
